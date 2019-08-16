4519736d9c52b246c1bded5a0870e264aefadcd6
vincaslt <vincaslt@gmail.com>
2019-08-16 15:48:21 +0300

    wip payment sections

--- src/app/containers/SignupContainer/SignupContainer.js
+++ src/app/containers/SignupContainer/SignupContainer.js
@@ -2,19 +2,23 @@ import React from 'react';
 import VpnLogo from 'react-components/components/logo/VpnLogo';
 import { Wizard } from 'react-components';
 import PlansSection from './PlansSection/PlansSection';
+import EmailSection from './EmailSection/EmailSection';
+import PaymentDetailsSection from './PaymentDetailsSection/PaymentDetailsSection';
 
 const SignupContainer = () => {
     return (
         <>
-            <div className="flex color-white flex-items-center bg-black flex-spacebetween pt1 pb1 pl2 pr2">
+            <header className="flex header color-white flex-items-center bg-black flex-spacebetween pt1 pb1 pl2 pr2">
                 <VpnLogo />
                 <div className="mw650p flex-item-fluid">
                     <Wizard steps={['Plan', 'Email', 'Verification', 'Finish']} />
                 </div>
-            </div>
-            <main className="flex-item-fluid main-area main-full">
+            </header>
+            <main className="flex-item-fluid main-area">
                 <div className="container-section-sticky">
                     <PlansSection />
+                    <EmailSection />
+                    <PaymentDetailsSection />
                 </div>
             </main>
         </>
