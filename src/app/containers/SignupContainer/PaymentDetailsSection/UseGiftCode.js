36d2b461b416d3dd472016c7cd388e87cb1279ed
vincaslt <vincaslt@gmail.com>
2019-08-19 17:19:23 +0300

    Some toggleable elements

--- src/app/containers/SignupContainer/PaymentDetailsSection/UseGiftCode.js
+++ src/app/containers/SignupContainer/PaymentDetailsSection/UseGiftCode.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { LinkButton } from 'react-components';
+import React, { useState } from 'react';
+import { LinkButton, Button, Input } from 'react-components';
 import { c } from 'ttag';
 
 const UseGiftCode = () => {
-    return <LinkButton className="mr1">{c('Action').t`Use gift code`}</LinkButton>;
+    const [isEditing, setEditing] = useState(false);
+
+    const handleClickEdit = () => setEditing(true);
+
+    if (isEditing) {
+        return (
+            <div className="flex mr1">
+                <Input className="flex-item-fluid mr0-5" placeholder={c('Placeholder').t`AAAA-BBBB-CCCC`} />
+                <Button>{c('Action').t`Apply`}</Button>
+            </div>
+        );
+    }
+
+    return <LinkButton onClick={handleClickEdit} className="mr1">{c('Action').t`Use gift code`}</LinkButton>;
 };
 
 export default UseGiftCode;
