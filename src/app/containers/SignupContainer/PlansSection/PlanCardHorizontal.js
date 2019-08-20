72922be5ccff19a7bb847ebb1bb505d60a65d6c6
vincaslt <vincaslt@gmail.com>
2019-08-20 14:29:28 +0300

    More toggling logic...

--- src/app/containers/SignupContainer/PlansSection/PlanCardHorizontal.js
+++ src/app/containers/SignupContainer/PlansSection/PlanCardHorizontal.js
@@ -6,7 +6,11 @@ import PlanPrice from './PlanPrice';
 
 const PlanCardHorizontal = ({ active, plan, isAnnual, onClick }) => {
     return (
-        <div className={classnames(['plan-card w100', active && 'plan-card--active'])} role="button" onClick={onClick}>
+        <div
+            className={classnames(['plan-card w100 border-top', active && 'plan-card--active'])}
+            role="button"
+            onClick={onClick}
+        >
             <div>
                 <strong className="biggest">{plan.title}</strong>
                 <div>{plan.description}</div>
