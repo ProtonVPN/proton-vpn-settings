b7e49f2c58426de027590574e8237390bff12ec4
vincaslt <vincaslt@gmail.com>
2019-08-21 14:56:50 +0300

    Email and sms verification

---
+++ src/app/containers/SignupContainer/VerificationStep/PhoneInput.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Field, Row, PrimaryButton, TelInput } from 'react-components';
+import { c } from 'ttag';
+
+const PhoneInput = ({ onSendClick, loading }) => {
+    const [phone, setPhone] = useState('');
+
+    const handleChangePhone = ({ target }) => setPhone(target.value);
+    const handleSendClick = () => onSendClick(phone);
+
+    return (
+        <Row>
+            <Field className="mr1">
+                <TelInput value={phone} onChange={handleChangePhone} placeholder="(201) 555-0123" />
+            </Field>
+            <PrimaryButton loading={loading} onClick={handleSendClick}>{c('Action').t`Send`}</PrimaryButton>
+        </Row>
+    );
+};
+
+PhoneInput.propTypes = {
+    onSendClick: PropTypes.func.isRequired,
+    loading: PropTypes.bool
+};
+
+export default PhoneInput;
