d1a4a49801192414f6ce7e98d626ae49f248c217
Richard <richard@protonmail.com>
2019-09-02 16:42:52 +0200

    Review form to use useLoading

--- src/app/components/sections/redeem/RedeemCouponForm.js
+++ src/app/components/sections/redeem/RedeemCouponForm.js
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { c } from 'ttag';
 import { withRouter } from 'react-router-dom';
-import { useApi, useConfig, PrimaryButton, Input } from 'react-components';
+import { useApi, useConfig, PrimaryButton, Input, useLoading } from 'react-components';
 import { queryCheckVerificationCode } from 'proton-shared/lib/api/user';
 import { checkSubscription } from 'proton-shared/lib/api/payments';
 import { TOKEN_TYPES } from 'proton-shared/lib/constants';
@@ -10,6 +10,7 @@ import { API_CUSTOM_ERROR_CODES } from 'proton-shared/lib/errors';
 
 const RedeemCouponForm = () => {
     const api = useApi();
+    const [loading, withLoading] = useLoading();
     const { CLIENT_TYPE } = useConfig();
     const [couponCode, setCouponCode] = useState();
     const [validationError, setValidationError] = useState();
@@ -18,42 +19,39 @@ const RedeemCouponForm = () => {
         setCouponCode(value);
     }, []);
 
-    const handleSubmit = useCallback(
-        async (e) => {
-            e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-            try {
-                const {
-                    Plans: [{ ID, Currency, Cycle, Quantity }]
-                } = await api(queryCheckVerificationCode(couponCode, TOKEN_TYPES.COUPON, CLIENT_TYPE));
+        try {
+            const {
+                Plans: [{ ID, Currency, Cycle, Quantity }]
+            } = await api(queryCheckVerificationCode(couponCode, TOKEN_TYPES.COUPON, CLIENT_TYPE));
 
-                const response = await api(
-                    checkSubscription({
-                        CouponCode: couponCode,
-                        Currency,
-                        Cycle,
-                        PlanIDs: {
-                            [ID]: Quantity
-                        }
-                    })
-                );
+            const response = await api(
+                checkSubscription({
+                    CouponCode: couponCode,
+                    Currency,
+                    Cycle,
+                    PlanIDs: {
+                        [ID]: Quantity
+                    }
+                })
+            );
 
-                history.push('/signup', response);
-            } catch (e) {
-                if (!e.data) {
-                    throw e;
-                } else if (e.data.Code === API_CUSTOM_ERROR_CODES.TOKEN_INVALID) {
-                    setValidationError('Invalid coupon. Please try again.');
-                } else {
-                    setValidationError(e.data.Error);
-                }
+            history.push('/signup', response);
+        } catch (e) {
+            if (!e.data) {
+                throw e;
+            } else if (e.data.Code === API_CUSTOM_ERROR_CODES.TOKEN_INVALID) {
+                setValidationError('Invalid coupon. Please try again.');
+            } else {
+                setValidationError(e.data.Error);
             }
-        },
-        [couponCode]
-    );
+        }
+    };
 
     return (
-        <form className="redeem-form w100 col flex flex-column pb2" onSubmit={handleSubmit}>
+        <form className="redeem-form w100 col flex flex-column pb2" onSubmit={(e) => withLoading(handleSubmit(e))}>
             <Input
                 className="redeem-coupon-code-input"
                 placeholder={c('Placeholder').t`Enter coupon code`}
@@ -62,6 +60,7 @@ const RedeemCouponForm = () => {
                 errorZoneClassName="bg-global-warning color-white aligncenter p0-5"
             />
             <PrimaryButton
+                loading={loading}
                 type="submit"
                 className="redeem-submit-button pm-button--noborder uppercase bold mt2 pt1 pb1"
             >
