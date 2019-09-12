4501ea6af805cc5620ad3d74180b391c90e76827
Richard <richard@protonmail.com>
2019-09-12 21:43:43 +0200

    Review

--- src/app/containers/SignupContainer/MobileRedirectionStep/MobileRedirectionStep.js
+++ src/app/containers/SignupContainer/MobileRedirectionStep/MobileRedirectionStep.js
@@ -5,18 +5,16 @@ import { SubTitle, Href, Icon, Paragraph } from 'react-components';
 
 const MobileRedirectionStep = ({ model }) => {
     return (
-        <div className="pt2 mb2">
-            <div className="aligncenter">
-                <SubTitle>{c('Title').t`Account created`}</SubTitle>
-                <Icon name="on" className="mb2" fill="success" size={100} />
-                <Paragraph className="mb2">{c('Info')
-                    .t`Your account has been successfully created. Please press the "Close" button to be taken back to the app.`}</Paragraph>
-                <Href
-                    className="pm-button pm-button--primary pm-button--large bold"
-                    url={`protonvpn://registered?username=${model.username}`}
-                    target="_top"
-                >{c('Link').t`Close`}</Href>
-            </div>
+        <div className="pt2 mb2 aligncenter">
+            <SubTitle>{c('Title').t`Account created`}</SubTitle>
+            <Icon name="on" className="mb2" fill="success" size={100} />
+            <Paragraph className="mb2">{c('Info')
+                .t`Your account has been successfully created. Please press the "Close" button to be taken back to the app.`}</Paragraph>
+            <Href
+                className="pm-button pm-button--primary pm-button--large bold"
+                url={`protonvpn://registered?username=${model.username}`}
+                target="_top"
+            >{c('Link').t`Close`}</Href>
         </div>
     );
 };
