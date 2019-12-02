c1a2d1e4ebc2f036aa4a5914aace0e4fa97e1029
Richard <richard@protonmail.com>
2019-12-02 09:20:17 +0100

    Move more logic in BlackFridayNavbarLink

--- src/app/components/layout/PrivateHeader.js
+++ src/app/components/layout/PrivateHeader.js
@@ -1,54 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-    MainLogo,
-    TopNavbar,
-    Hamburger,
-    BlackFridayNavbarLink,
-    SubscriptionModal,
-    usePlans,
-    useModals,
-    VPNBlackFridayModal,
-    useSubscription
-} from 'react-components';
+import { MainLogo, TopNavbar, Hamburger, BlackFridayNavbarLink, VPNBlackFridayModal } from 'react-components';
 
 const PrivateHeader = ({ location, expanded, onToggleExpand }) => {
-    const [plans] = usePlans();
-    const [subscription] = useSubscription();
-    const { createModal } = useModals();
-
-    const handleSelect = ({ planIDs = [], cycle, currency, couponCode }) => {
-        const plansMap = planIDs.reduce((acc, planID) => {
-            const { Name } = plans.find(({ ID }) => ID === planID);
-            acc[Name] = 1;
-            return acc;
-        }, Object.create(null));
-
-        createModal(
-            <SubscriptionModal
-                plansMap={plansMap}
-                customize={false}
-                subscription={subscription}
-                cycle={cycle}
-                currency={currency}
-                coupon={couponCode}
-            />
-        );
-    };
-
-    const handleClick = () => {
-        if (location.pathname === '/dashboard') {
-            createModal(<VPNBlackFridayModal plans={plans} onSelect={handleSelect} />);
-        }
-    };
-
     return (
         <header className="header flex flex-nowrap reset4print">
             <MainLogo url="/account" className="nomobile" />
             <Hamburger expanded={expanded} onToggle={onToggleExpand} />
             <div className="searchbox-container nomobile" />
             <TopNavbar>
-                <BlackFridayNavbarLink to="/dashboard" onClick={handleClick} />
+                <BlackFridayNavbarLink
+                    to="/dashboard"
+                    location={location}
+                    getModal={({ plans, onSelect }) => <VPNBlackFridayModal plans={plans} onSelect={onSelect} />}
+                />
             </TopNavbar>
         </header>
     );
